30f19f6a9b75f72fb5dfc1f2cd12a7159549cf5f
gracebir <grace.devolop@gmail.com>
2024-02-09 22:25:11 +0200

    login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,87 @@
+import 'package:firebase_chat_app/components/my_button.dart';
+import 'package:firebase_chat_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            //logo
+            Icon(
+              Icons.message,
+              color: Theme.of(context).colorScheme.primary,
+              size: 60,
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            Text(
+              "Welcome back you've been missed",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 16, color: Theme.of(context).colorScheme.primary),
+            ),
+            // email
+            const SizedBox(
+              height: 25,
+            ),
+            MyTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            // password
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _passwordController,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            // button login
+            MyButton(text: "Login", onTap: () {}),
+            const SizedBox(
+              height: 15,
+            ),
+            // register now link
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Not a member? ",
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+                Text(
+                  "Register now",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.primary),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
