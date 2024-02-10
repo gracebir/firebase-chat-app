577e5cc6939d02729022ea09fee1d4599dd918c1
gracebir <grace.devolop@gmail.com>
2024-02-10 19:46:38 +0200

    list all the user

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_chat_app/components/user_tile.dart';
+import 'package:firebase_chat_app/pages/chat_page.dart';
 import 'package:firebase_chat_app/services/auth/auth_service.dart';
 import 'package:firebase_chat_app/components/my_drawer.dart';
+import 'package:firebase_chat_app/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final ChatService _chatService = ChatService();
 
+  HomePage({super.key});
   void logout() async {
     AuthService authService = AuthService();
     await authService.signOut();
@@ -20,6 +24,46 @@ class HomePage extends StatelessWidget {
         ],
       ),
       drawer: const MyDrawer(),
+      body: _buidUserList(),
     );
+  }
+
+  Widget _buidUserList() {
+    return StreamBuilder(
+        stream: _chatService.getUsersStream(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Error");
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading...");
+          }
+
+          return ListView(
+            children: snapshot.data!
+                .map<Widget>(
+                    (userData) => _buildUserListItem(userData, context))
+                .toList(),
+          );
+        });
+  }
+}
+
+Widget _buildUserListItem(Map<String, dynamic> userData, BuildContext context) {
+  final AuthService authService = AuthService();
+  if (userData["email"] != authService.getCurrentUse()!.email) {
+    return UserTile(
+        text: userData["email"],
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ChatPage(
+                        receiveEmail: userData['email'],
+                      )));
+        });
+  } else {
+    return Container();
   }
 }
