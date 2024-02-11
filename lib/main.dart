528e15c98b21749c30e0079907786c38855c2934
gracebir <grace.devolop@gmail.com>
2024-02-11 07:48:25 +0200

    switching theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 // import 'package:firebase_chat_app/pages/login_page.dart';
 import 'package:firebase_chat_app/services/auth/auth_gate.dart';
 import 'package:firebase_chat_app/firebase_options.dart';
-import 'package:firebase_chat_app/theme/light_mode.dart';
+import 'package:firebase_chat_app/theme/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: lightMode,
+      theme: Provider.of<ThemeProvider>(context).themeData,
       debugShowCheckedModeBanner: false,
       home: const AuthGate(),
     );
--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_chat_app/theme/theme_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
@@ -13,6 +16,29 @@ class SettingPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
         foregroundColor: Colors.grey,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.all(25),
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "Dark theme",
+              style: TextStyle(fontSize: 17),
+            ),
+            CupertinoSwitch(
+                value: Provider.of<ThemeProvider>(context, listen: false)
+                    .isDarkMode,
+                onChanged: (value) {
+                  Provider.of<ThemeProvider>(context, listen: false)
+                      .toggleTheme();
+                })
+          ],
+        ),
       ),
     );
   }
